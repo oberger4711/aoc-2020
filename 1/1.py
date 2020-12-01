32a8177a3e6da093d18d297579d5ef4b83019ee1
oberger <ole.k.berger@t-online.de>
2020-12-01 21:36:12 +0100

    Speedup 1

--- 1/1.py
+++ 1/1.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python3
 
+from measure import measure
+
 numbers = [int(line) for line in open("input.txt")]
 
+@measure
 def part1():
     lookup = [False for _ in range(2020)]
     for n in numbers:
         lookup[n] = True
     for n in numbers:
         if lookup[2020 - n]:
-            print("Part 1: {}".format(n * (2020 - n)))
-            return
+            return n * (2020 - n)
 
-def part2():
-    # O(n^3) is fast enough for only 200 numbers.
+@measure
+def part2_on3():
     for a in numbers:
         for b in numbers:
             for c in numbers:
                 if a + b + c == 2020:
-                    print("Part 2: {}".format(a * b * c))
-                    return
+                    return a * b * c
 
-part1()
-part2()
+@measure
+def part2_on2():
+    lookup = [False for _ in range(2020)]
+    for n in numbers:
+        lookup[n] = True
+    for a in numbers:
+        for b in numbers:
+            c = 2020 - a - b
+            if c > 0 and lookup[c]:
+                return a * b * c
+
+print("Part 1:         {}".format(part1()))
+print("Part 2 (O(n^3): {}".format(part2_on3()))
+print("Part 2 (O(n^2): {}".format(part2_on2()))
