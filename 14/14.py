25beb9ac6db9090b249f321062bf9aca0d772cb1
oberger <ole.k.berger@t-online.de>
2020-12-14 22:51:23 +0100

    Solve 14 part 2

--- 14/14.py
+++ 14/14.py
@@ -4,6 +4,7 @@ import re
 
 lines = [l.rstrip("\n") for l in open("input.txt")]
 #lines = [l.rstrip("\n") for l in open("input_test_1.txt")]
+#lines = [l.rstrip("\n") for l in open("input_test_2.txt")]
 
 re_mem = re.compile("^mem\[(\d+)\] = (\d+)$")
 re_mask = re.compile("^mask = ([X01]+)$")
@@ -35,7 +36,32 @@ def part1():
     return sum(mem.values())
 
 def part2():
-    pass
+    mask_or = 0
+    mask_and = 0xFFFFFFFFFF
+    floating_digits = []
+    mem = {}
+    for l in lines:
+        match_mem = re_mem.match(l)
+        if match_mem:
+            # Memory assignment
+            addr_str, val_str = match_mem.groups()
+            addr_base = (int(addr_str) | mask_or) & mask_and
+            val = int(val_str)
+            for code in range(2 ** len(floating_digits)):
+                addr = addr_base
+                for i, fd in enumerate(reversed(floating_digits)):
+                    addr |= ((code >> i) & 1) << fd
+                #print("{:b} --mask({})--> {:b}".format(addr_base, code, addr))
+                #print("mem[{}] = {}".format(addr, val_str))
+                mem[addr] = val
+        else:
+            # Mask assignment
+            match_mask = re_mask.match(l)
+            mask_str = match_mask.groups()[0]
+            mask_or = int(mask_str.replace("X", "1"), base=2)
+            mask_and = int(mask_str.replace("0", "1").replace("X", "0"), base=2)
+            floating_digits = [len(mask_str) - 1 - i for i, c in enumerate(mask_str) if c == "X"]
+    return sum(mem.values())
 
 print("Part 1: {}".format(part1()))
 print("Part 2: {}".format(part2()))
