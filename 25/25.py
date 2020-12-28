77d921b26f18aa2dd01e5e0ed7d5fe3c94a5b498
oberger <ole.k.berger@t-online.de>
2020-12-28 15:29:43 +0100

    Cleanup

--- 25/25.py
+++ 25/25.py
@@ -6,19 +6,20 @@ def transform(subj_num, secret_loop_size):
         v = (v * subj_num) % 20201227
     return v
 
+def find_secret(subj_num, pk):
+    v = 1
+    iterations = 0
+    while True:
+        if v == pk: return iterations
+        v = (v * subj_num) % 20201227
+        iterations += 1
+
 def part1():
     subj_num = 7
-    def find_secret(pk):
-        v = 1
-        iterations = 0
-        while True:
-            if v == pk: return iterations
-            v = (v * subj_num) % 20201227
-            iterations += 1
     pk_a = 15113849
     pk_b = 4206373
-    secret_a = find_secret(pk_a)
-    secret_b = find_secret(pk_b)
+    secret_a = find_secret(subj_num, pk_a)
+    secret_b = find_secret(subj_num, pk_b)
     enc_key_a = transform(pk_b, secret_a)
     enc_key_b = transform(pk_a, secret_b)
     if enc_key_a != enc_key_b:
